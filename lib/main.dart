3d4f0895f1741b1edfa2be47a379d3af138d0c0c
Reynaldo-LW <reynaldo@littlewallet.in>
2023-10-25 20:28:06 +0700

    Commit Test apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,177 @@
+import 'dart:convert';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() => runApp(const MyApp());
+
+class Contact {
+  final String id;
+  String firstName;
+  String lastName;
+  String email;
+  String dob;
+
+  Contact({
+    required this.id,
+    required this.firstName,
+    required this.lastName,
+    this.email = "",
+    this.dob = "",
+  });
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return MaterialApp( debugShowCheckedModeBanner: false,
+      home: ContactListScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ContactListScreen extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _ContactListScreenState createState() => _ContactListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ContactListScreenState extends State<ContactListScreen> {
+  final RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+  List<Contact> contacts = [];
 
-  void _incrementCounter() {
+  Future<void> _loadContacts() async {
+  try {
+    final jsonData = await rootBundle.loadString('lib/assets/data.json');
+    final List<dynamic> jsonList = json.decode(jsonData);
+    final List<Contact> loadedContacts = jsonList
+        .map((contact) => Contact(
+              id: contact['id'],
+              firstName: contact['firstName'],
+              lastName: contact['lastName'],
+              email: contact['email'] ?? '',
+              dob: contact['dob'] ?? '',
+            ))
+        .toList();
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      contacts = loadedContacts;
     });
+  } catch (e) {
+    print('Error loading contacts: $e');
+  }
+}
+
+  @override
+  void initState() {
+    _loadContacts();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Contact List'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      body: SmartRefresher(
+        controller: _refreshController,
+        onRefresh: _loadContacts,
+        child: GridView.builder(
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+            return CircleAvatar(
+      backgroundColor: Colors.orange,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('${contacts[index].firstName} ${contacts[index].lastName}', textAlign: TextAlign.center,style: const TextStyle(color: Colors.blueAccent),),
+         
+        ],
+      ),
+    );
+      }, 
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 8.0, 
+        mainAxisSpacing: 8.0, 
+      ),
+    ),
+  ),
+    );
+  }
+}
+
+class ContactEditScreen extends StatefulWidget {
+  final Contact contact;
+  final Function(Contact) onSave;
+
+  const ContactEditScreen({super.key, required this.contact,required this.onSave});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ContactEditScreenState createState() => _ContactEditScreenState();
+}
+
+class _ContactEditScreenState extends State<ContactEditScreen> {
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController dobController = TextEditingController();
+
+  @override
+  void initState() {
+    firstNameController.text = widget.contact.firstName;
+    lastNameController.text = widget.contact.lastName;
+    emailController.text = widget.contact.email;
+    dobController.text = widget.contact.dob;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Edit Contact'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              TextFormField(
+                controller: firstNameController,
+                decoration: const InputDecoration(labelText: 'First Name'),
+              ),
+              TextFormField(
+                controller: lastNameController,
+                decoration: const InputDecoration(labelText: 'Last Name'),
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
+              ),
+              TextFormField(
+                controller: dobController,
+                decoration: const InputDecoration(labelText: 'Date of Birth'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  widget.contact.firstName = firstNameController.text;
+                  widget.contact.lastName = lastNameController.text;
+                  widget.contact.email = emailController.text;
+                  widget.contact.dob = dobController.text;
+                  widget.onSave(widget.contact);
+                  Navigator.pop(context);
+                },
+                child: const Text('Save'),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
